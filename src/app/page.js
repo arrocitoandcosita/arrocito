13213e327e5ab0965ec1a748707469d794fe0905
dav <alinehartig.oficial@gmail.com>
2023-12-18 19:33:11 -0500

    Segunda targeta y modificacion de props css

--- src/app/page.js
+++ src/app/page.js
@@ -12,30 +12,40 @@ export default function Home() {
           <li className={styles.card}  style={{ '--index': 1 }} >
             <Card urlImage = {"https://lh3.googleusercontent.com/drive-viewer/AEYmBYSY63IPrhLyOAzaGFzPK_VqRYFJtZraz6gSnbaioWRHPt6T0AAWiYWwxtso-fIk-A_0dkG9Xgk6Rz06Pogr5McaW15iyA=s1600"}
             backgroundColor ="rgb(36,42,59)"
-            texColor = "rgb(255,255,255)"
+            texColor = "rgb(255, 255, 255)"
             index = {1}
+            rigth = {true}
+            top = {true}
+            left = {false}
+            bottom = {false}
             text={'Seria tu sonrisa, seria tus mirada, seria tu cuerpo, seria tu espíritu, seria el cálido color del sol; alguno de tantos detalles que tienes que me cautivaron y no logro permitir que te alejes de mis pensamientos así lejos estés.'}     ></Card>
           </li>
           <li className={styles.card}  style={{ '--index': 2 }} >
             <Card 
-              urlImage = {""}
+              urlImage = {"https://drive.google.com/uc?export=download&id=1aAtxLxJlsDd4y9him_P1eyVN-O37QWX0"}
               backgroundColor="white"
               texColor = "rgb(40, 38, 38)"
-              text={'Espero ancioso por ver nuestro segundo encuentro y conocer mas de eso que me gusta'}
+              backgroundColorText={true}
+              rigth = {false}
+              top = {true}
+              left = {true}
+              bottom = {false}
+              text={'Dicen que la perfección no existe, pero me atrevo a decir que encontrarte podría ser sinónimo de ella. Puedo relatar con detalle aquella ventana de madera con su aroma a humedad, el frío que se deslizaba hacia el interior de la habitación y el vapor del café que nos acompañaba. Sin embargo, no puedo describir lo que sentía al tocar tu mano, acariciar tu cabello o al perderme en la profundidad de tus ojos, porque no existen palabras que puedan describirlo.'}
               index = {2}
             ></Card>
           </li>
           <li className={styles.card}  style={{ '--index': 3 }} >
             <Card 
-              urlImage = {""}
-              backgroundColor="transparent"
-              texColor = "transparent"
+              urlImage = {null}
+              backgroundColor="white"
+              texColor = "rgb(40, 38, 38)"
+              text={'Espero ancioso por ver nuestro tercer encuentro y conocer mas de lo que te gusta'}
               index = {3}
             ></Card>
           </li>
           <li className={styles.card}  style={{ '--index': 4 }} >
             <Card 
-              urlImage = {""}
+              urlImage = {null}
               backgroundColor="transparent"
               texColor = "transparent"
               index = {4}
@@ -43,9 +53,9 @@ export default function Home() {
           </li>
           <li className={styles.card}  style={{ '--index': 5 }} >
             <Card 
-              urlImage = {""}
+              urlImage={null}
               backgroundColor="transparent"
-              texColor = "transparent"
+              texColor="transparent"
               index = {5}
             ></Card>
           </li>
--- src/app/ui/card/index.jsx
+++ src/app/ui/card/index.jsx
@@ -1,15 +1,32 @@
 import { Typography } from '@mui/material'
 import styles from './card.module.css'
 
-export default function Card ({urlImage,backgroundColor,index,texColor,text}){
+export default function Card ({urlImage,backgroundColor,index,texColor,text,backgroundColorText,right,top, left, bottom }){
     return(
-        <div className={`${styles.description}`} style={{backgroundColor:backgroundColor,'--index': index}}>
-            <Typography color={texColor} variant="h2" >
+        <div className={`${styles.description}`} style={{
+            backgroundColor:backgroundColor,
+            '--index': index,
+
+
+            
+            }}>
+            <Typography 
+                color={texColor} 
+                variant="h2" 
+                sx={{
+                    boxShadow: backgroundColorText ? 'rgba(0, 0, 0, 0.24) 0px 3px 8px' : 'none',
+                    backgroundColor: backgroundColorText ? 'rgba(255,255,255,0.5)' : 'transparent',
+                    right: right ? 0 : "inherit",
+                    top: top ? 0 : "inherit",
+                    left : left ? 0 : "inherit",
+                    bottom : bottom ? 0 : "inherit"
+                }}
+                >
                 {text}
             </Typography>
-            <img
+            {urlImage && <img
                 src={urlImage}
-            />
+            />}
         </div>
     )
 }   
